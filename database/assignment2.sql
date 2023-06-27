7cf7f5dd2197c18c7218042ce5818b9057f578f4
margoaston <margo.aston@yahoo.com>
2023-06-26 17:29:38 -0700

    added classification page

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,33 +1,38 @@
--- Insert new record
+-- 1 Insert new record
 INSERT INTO account
 	(account_firstname, account_lastname, account_email, account_password)
 VALUES
 	('Tony', 'Stark', 'tony@starkent.com', 'Iam!ronM@n')
+;
 
--- Modify record
+-- 2 Modify record
 UPDATE
 	account
 SET
 	account_type = 'Admin'
 WHERE
 	account_id = 2
+;
 
--- Delete record
+-- 3 Delete record
 DELETE
 FROM
 	account
 WHERE
 	account_id = 2
+;
 
--- Modify "GM Hummer"  
+-- 4 Modify "GM Hummer"  
 UPDATE
 	inventory
 SET
 	inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE
 	inv_id = 10
+;
 
--- Update file paths
+
+-- 5 Query inventory belonging to sport category
 SELECT
 	inv_make,
 	inv_model,
@@ -37,4 +42,13 @@ FROM
 	
 INNER JOIN classification
 	ON inventory.classification_id = classification.classification_id
-WHERE classification.classification_name = 'Sport';
+WHERE classification.classification_name = 'Sport'
+;
+
+
+--6 Update file path
+UPDATE
+	inventory
+SET
+	inv_image = REPLACE(inv_image, 'images/', 'images/vehicle/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, 'images/', 'images/vehicle/')
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 
@@ -28,6 +29,9 @@ app.use(require("./routes/static"))
 
 // Index route
 app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
